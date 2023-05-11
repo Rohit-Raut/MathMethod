3e68e2b9fa49b309b64c3385fa152c00049b6159
Rohit Raut <61349517+Rohit-Raut@users.noreply.github.com>
2023-05-11 16:35:03 -0500

    Determinant calculation
    Helps to determine the determinant of the matrix using co-factor method

---
+++ co-factor.py
@@ -0,0 +1,65 @@
+#https://github.com/weijie-chen/Linear-Algebra-With-Python/blob/master/Chapter%203%20-%20Determinant.ipynb
+
+
+def get_matrix_dimension():
+    while True:
+        try:
+            dimension = int(input("Enter the number of rows/columns for the square matrix: "))
+            if dimension <= 0:
+                print("Please enter a positive integer for the matrix dimension.")
+            else:
+                return dimension
+        except ValueError:
+            print("Invalid input. Please enter a positive integer for the matrix dimension.")
+
+
+def get_matrix_entries(dimension):
+    matrix = []
+    for i in range(dimension):
+        row = []
+        for j in range(dimension):
+            while True:
+                try:
+                    entry = int(input(f"Enter the element A({i+1},{j+1}): "))
+                    row.append(entry)
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter an integer for the matrix element.")
+        matrix.append(row)
+    return matrix
+
+
+def calculate_determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    determinant = 0
+    for j in range(len(matrix)):
+        sign = (-1) ** j
+        cofactor = sign * matrix[0][j] * calculate_determinant(get_submatrix(matrix, 0, j))
+        determinant += cofactor
+
+    return determinant
+
+
+def get_submatrix(matrix, row_to_exclude, col_to_exclude):
+    submatrix = []
+    for i in range(1, len(matrix)):
+        row = []
+        for j in range(len(matrix)):
+            if i != row_to_exclude and j != col_to_exclude:
+                row.append(matrix[i][j])
+        if row:
+            submatrix.append(row)
+    return submatrix
+
+
+def main():
+    dimension = get_matrix_dimension()
+    matrix = get_matrix_entries(dimension)
+    determinant = calculate_determinant(matrix)
+    print("The determinant of the matrix is:", determinant)
+
+
+if __name__ == "__main__":
+    main()
